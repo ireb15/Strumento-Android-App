190113c485af96d75ddfdc3fcb8e2ff4a0a456e2
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-05-26 22:39:17 +1200

    Created Instrument class (Parent class)

---
+++ instrument_app/app/src/main/java/Instrument.java
@@ -0,0 +1,48 @@
+public abstract class Instrument {
+    private String title;
+    private int price;
+    private String brand;
+    private String colour;
+    private String condition;
+    private String location;
+    private String description;
+    private int views;
+
+    public Instrument(String title, int price, String brand, String colour, String condition, String location, String description) {
+        this.title = title;
+        this.price = price;
+        this.brand = brand;
+        this.colour = colour;
+        this.condition = condition;
+        this.location = location;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
