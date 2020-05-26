abadd3d126303dd998ec20a2bb18ac4f11fd114a
Cassandra D'Souza <cdso559@aucklanduni.ac.nz>
2020-05-26 23:47:27 +1200

    Child Classes Complete

--- instrument_app/app/src/main/java/Instrument.java
+++ instrument_app/app/src/main/java/Instrument.java
@@ -1,3 +1,6 @@
+//Instrument App Classes
+
+// Instrument Class
 public abstract class Instrument {
     private String title;
     private int price;
@@ -6,9 +9,12 @@ public abstract class Instrument {
     private String condition;
     private String location;
     private String description;
+    private int id;
     private int views;
 
-    public Instrument(String title, int price, String brand, String colour, String condition, String location, String description) {
+    public Instrument(String title, int price, String brand, String colour,
+                      String condition, String location, String description, int id) {
+
         this.title = title;
         this.price = price;
         this.brand = brand;
@@ -16,6 +22,7 @@ public abstract class Instrument {
         this.condition = condition;
         this.location = location;
         this.description = description;
+        this.id = id;
     }
 
     public String getTitle() {
@@ -48,5 +55,64 @@ public abstract class Instrument {
 
     public int getViews() {
         return views;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+// Guitar Class
+abstract class Guitar extends Instrument {
+    public Guitar (String title, int price, String brand, String colour,
+                   String condition, String location, String description, int id)
+    {
+        super(title,  price,  brand, colour, condition, location, description, id) ;
+    }
+
+}
+
+// Acoustic Guitar Class
+class AcousticGuitar extends Instrument {
+    public AcousticGuitar (String title, int price, String brand, String colour,
+                    String condition, String location, String description, int id)
+    {
+        super(title,  price,  brand, colour, condition, location, description, id) ;
+    }
+}
+
+// Electric Guitar Class
+class ElectricGuitar extends Instrument {
+    public ElectricGuitar (String title, int price, String brand, String colour,
+                           String condition, String location, String description, int id)
+    {
+        super(title,  price,  brand, colour, condition, location, description, id) ;
+    }
+}
+
+// Piano Class
+class Piano extends Instrument {
+    public Piano (String title, int price, String brand, String colour,
+                   String condition, String location, String description, int id)
+    {
+        super(title,  price,  brand, colour, condition, location, description, id) ;
+    }
+}
+
+// Ukulele Class
+class Ukulele extends Instrument {
+    public Ukulele (String title, int price, String brand, String colour,
+                  String condition, String location, String description, int id)
+    {
+        super(title,  price,  brand, colour, condition, location, description, id) ;
+    }
+}
+
+// Drum Class
+class Drum extends Instrument {
+    public Drum (String title, int price, String brand, String colour,
+                  String condition, String location, String description, int id)
+    {
+        super(title,  price,  brand, colour, condition, location, description, id) ;
     }
 }
